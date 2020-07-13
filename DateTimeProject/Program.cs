b1d33a2826dee60f76c554360ce0a9dc7b1fcb17
Justin Muench <55668563+muench-develops@users.noreply.github.com>
2020-07-13 17:48:15 +0200

    Projektdateien hinzufügen.

---
+++ DateTimeProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DateTimeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Datetime Now, wenn möglich immer vermeiden, da local Zeit
+            var now = DateTime.Now;
+
+            TimeZoneInfo sydneyTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("E. Australia Standard Time");
+
+            var sydneyTime = TimeZoneInfo.ConvertTime(now, sydneyTimeZoneInfo);
+
+            Console.WriteLine(now);
+            Console.WriteLine(sydneyTime);
+
+        }
+    }
+}
